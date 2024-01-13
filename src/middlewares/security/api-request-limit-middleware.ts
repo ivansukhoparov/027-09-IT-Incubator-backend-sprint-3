531fcb010d2def7756791f99f775b97c93e58da4
ivansukhoparov <rbcdaaa@gmail.com>
2024-01-13 15:00:02 +0100

    remove magic numbers from apiRequestLimitMiddleware

--- src/middlewares/security/api-request-limit-middleware.ts
+++ src/middlewares/security/api-request-limit-middleware.ts
@@ -1,13 +1,14 @@
 import {NextFunction, Request, Response} from "express";
 import {ApiRequestsRepository} from "../../repositories/api-requests-repository";
 import {HTTP_STATUSES} from "../../utils/comon";
+import {settings} from "../../settings";
 
 
 export const apiRequestLimitMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const ip = req.ip || "unknown"
     await ApiRequestsRepository.writeRequest(ip, req.originalUrl)
-    const requestCount = await ApiRequestsRepository.countRequestByTime(ip, req.originalUrl, 10)
-    if (requestCount > 5) {
+    const requestCount = await ApiRequestsRepository.countRequestByTime(ip, req.originalUrl, settings.requestLimit.interval)
+    if (requestCount > settings.requestLimit.count) {
         res.sendStatus(HTTP_STATUSES.TOO_MANY_REQUESTS_429);
         return;
     } else {
--- src/settings.ts
+++ src/settings.ts
@@ -2,6 +2,19 @@
 const defaultSettings = {
     cors:{
         options:{}
+    },
+
+    refreshToken:{
+        expiredIn:"20s"
+    },
+
+    accessToken:{
+        expiredIn:"10s"
+    },
+
+    requestLimit:{
+        interval: 10, // interval in seconds
+        count:5 // count of requests in interval
     }
 }
 
@@ -11,7 +24,21 @@ const testSettings:typeof defaultSettings = {
             optionsSuccessStatus: 200,
             credentials: true,
         }
+    },
+
+    refreshToken:{
+        expiredIn:"200s"
+    },
+
+    accessToken:{
+        expiredIn:"100s"
+    },
+
+    requestLimit:{
+        interval: 10, // interval in seconds
+        count:5 // count of requests in interval
     }
+
 }
 
 export const settings = defaultSettings;
