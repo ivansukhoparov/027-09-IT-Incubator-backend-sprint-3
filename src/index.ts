dc119757e32cc325d3a269668e814bb858f24dd4
ivansukhoparov <rbcdaaa@gmail.com>
2024-01-14 01:09:09 +0100

    add trust proxy

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import {app} from "./app";
 import {port} from "./utils/comon";
 import {runDB} from "./db/db";
 
-
+app.set('trust proxy', true);
 const appStart = async () => {
     await runDB();
+
     app.listen(port, async () => {
         console.log(`app start on port ${port}`);
         console.log(`open in browser http://localhost:${port}`);
