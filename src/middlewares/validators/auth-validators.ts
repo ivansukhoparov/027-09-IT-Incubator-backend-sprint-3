be0ba442fdbe6533bad80579ad3ca511a9199140
ivansukhoparov <rbcdaaa@gmail.com>
2024-01-18 21:18:32 -0400

    add: email validation for auth router

--- src/middlewares/validators/auth-validators.ts
+++ src/middlewares/validators/auth-validators.ts
@@ -4,3 +4,5 @@ const loginOrEmailValidation = body("loginOrEmail").trim().isString().notEmpty()
 const passwordValidation = body("password").trim().isString().notEmpty();
 
 export const loginValidationChain = ()=>[loginOrEmailValidation,passwordValidation];
+
+export const emailValidator = body("email").trim().notEmpty().isString().isEmail();
