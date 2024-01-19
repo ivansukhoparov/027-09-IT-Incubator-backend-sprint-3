85f0c914f734b924680f7de8460bf132bae0a493
ivansukhoparov <rbcdaaa@gmail.com>
2024-01-19 02:14:24 +0100

    refactored tokens class

---
+++ src/common/utils/tokens.ts
@@ -0,0 +1,80 @@
+import {JwtToken} from "./adapters/jwt-tokens";
+import {settings} from "../../settings";
+import {RefreshTokenRepository} from "../../repositories/refresh-token-repository";
+import {RefreshTokenPayloadType} from "../../types/refresh-token/output";
+import {AccessTokenPayloadType} from "../../types/access-token/output";
+
+
+const secretKeyRefreshToken = process.env.REFRESH_TOKEN_SECRET_KEY!;
+const secretKeyAccessToken = process.env.ACCESS_TOKEN_SECRET_KEY!;
+const secretKeyRecoveryToken = process.env.PASS_RECOVERY_TOKEN_SECRET_KEY!;
+
+export class Tokens {
+
+    // Refresh token
+
+    static createRefreshToken = (userId: string, deviceId: string,
+                                 expiresIn: string = settings.refreshToken.expiredIn,
+                                 secretKey: string = secretKeyRefreshToken) => {
+
+        return JwtToken.create({userId: userId, deviceId: deviceId}, {expiresIn: expiresIn}, secretKey);
+    };
+    static verifyRefreshToken = async (token: string,
+                                       secretKey: string = secretKeyRefreshToken) => {
+        const isInBlackList = await RefreshTokenRepository.checkToken(token);
+        if (isInBlackList) {
+            return false;
+        }
+        return JwtToken.verify(token, secretKey);
+    }
+    static decodeRefreshToken = (token: string): RefreshTokenPayloadType | null => {
+        try {
+            const decodedToken: any = JwtToken.decode(token);
+            return {
+                userId: decodedToken.userId,
+                deviceId: decodedToken.deviceId,
+                iat: decodedToken.iat,
+                exp: decodedToken.exp
+            }
+        } catch (err) {
+            return null
+        }
+    }
+
+    // Access token
+
+    static createAccessToken = (userId: string,
+                                expiresIn: string = settings.accessToken.expiredIn,
+                                secretKey: string = secretKeyAccessToken) => {
+
+        return JwtToken.create({userId: userId}, {expiresIn: expiresIn}, secretKey);
+
+    };
+    static verifyAccessToken = async (token: string,
+                                      secretKey: string = secretKeyRefreshToken) => {
+        return JwtToken.verify(token, secretKey);
+    }
+    static decodeAccessToken = (token: string): AccessTokenPayloadType | null => {
+        try {
+            const decodedToken: any = JwtToken.decode(token);
+            return {
+                userId: decodedToken.userId,
+                iat: decodedToken.iat,
+                exp: decodedToken.exp
+            }
+        } catch (err) {
+            return null
+        }
+    }
+
+    // Password recovery token
+
+    static createPasswordRecoveryToken = (userId: string,
+                                          expiresIn: string = settings.accessToken.expiredIn,
+                                          secretKey: string = secretKeyRecoveryToken) => {
+
+        return JwtToken.create({userId: userId}, {expiresIn: expiresIn}, secretKey);
+
+    };
+
+}
