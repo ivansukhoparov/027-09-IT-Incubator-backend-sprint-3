8f21b82ca03daa456273c34416da6d1b8b32337e
ivansukhoparov <rbcdaaa@gmail.com>
2024-01-18 21:21:48 -0400

    refactor: rename class work with tokens

--- src/domains/security-service.ts
+++ src/domains/security-service.ts
@@ -1,4 +1,4 @@
-import {RefreshToken} from "../common/utils/refresh-token";
+import {Tokens} from "../common/utils/tokens";
 import {SecurityRepository} from "../repositories/security-repository";
 import {throws} from "assert";
 import {SecuritySessionUpdateType} from "../types/security/output";
@@ -9,7 +9,7 @@ import {compareSync} from "bcrypt";
 export class SecurityService {
     static async createAuthSession(refreshToken: string, deviceTitle: string, ip: string): Promise<boolean> {
 
-        const decodedToken = RefreshToken.decode(refreshToken);
+        const decodedToken = Tokens.decodeRefreshToken(refreshToken);
         if (!decodedToken) return false
 
         const newSession = {
@@ -30,7 +30,7 @@ export class SecurityService {
 
     static async updateAuthSession(refreshToken: string): Promise<boolean> {
 
-        const decodedToken = RefreshToken.decode(refreshToken);
+        const decodedToken = Tokens.decodeRefreshToken(refreshToken);
         if (!decodedToken) return false
 
         const updateData:SecuritySessionUpdateType = {
@@ -45,7 +45,7 @@ export class SecurityService {
     }
 
     static async terminateAuthSession(refreshToken:string, deviceId: string){
-        const decodedToken = RefreshToken.decode(refreshToken);
+        const decodedToken = Tokens.decodeRefreshToken(refreshToken);
         const session = await SecurityRepository.getSessionByDeviceId(deviceId);
         if (decodedToken?.userId!==session?.userId){
             return false;
@@ -54,7 +54,7 @@ export class SecurityService {
     }
 
     static async terminateCurrentSession(refreshToken:string){
-        const decodedToken = RefreshToken.decode(refreshToken);
+        const decodedToken = Tokens.decodeRefreshToken(refreshToken);
         const session = await SecurityRepository.getSessionByDeviceId(decodedToken!.deviceId);
         if (decodedToken?.userId!==session?.userId){
             return false;
@@ -63,7 +63,7 @@ export class SecurityService {
     }
 
     static async terminateOtherAuthSessions(refreshToken:string){
-        const decodedToken = RefreshToken.decode(refreshToken);
+        const decodedToken = Tokens.decodeRefreshToken(refreshToken);
         return await SecurityRepository.deleteSessionsExpectCurrent(decodedToken!.userId,decodedToken!.deviceId)
     }
 }
--- src/routers/security-router.ts
+++ src/routers/security-router.ts
@@ -1,7 +1,7 @@
 import {Request, Response, Router} from "express";
 import {HTTP_STATUSES} from "../utils/comon";
 import {SecurityQueryRepository} from "../repositories/security-query-repository";
-import {RefreshToken} from "../common/utils/refresh-token";
+import {Tokens} from "../common/utils/tokens";
 import {Params, RequestWithParams} from "../types/common";
 import {SecurityService} from "../domains/security-service";
 
@@ -9,12 +9,12 @@ export const securityRouter = Router()
 
 securityRouter.get("/devices", async (req: Request, res: Response) => {
     const refreshToken = req.cookies.refreshToken;
-    const isValid = await  RefreshToken.verify(refreshToken);
+    const isValid = await  Tokens.verifyRefreshToken(refreshToken);
     if (!isValid) {
         res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
         return;
     }
-    const decodedToken = RefreshToken.decode(refreshToken);
+    const decodedToken = Tokens.decodeRefreshToken(refreshToken);
 
     const sessions = await SecurityQueryRepository.getSessionByUserId(decodedToken!.userId)
     res.status(HTTP_STATUSES.OK_200).json(sessions);
@@ -22,7 +22,7 @@ securityRouter.get("/devices", async (req: Request, res: Response) => {
 
 securityRouter.delete("/devices/:id", async (req: RequestWithParams<Params>, res: Response) => {
     const refreshToken = req.cookies.refreshToken;
-    const isValid = await RefreshToken.verify(refreshToken);
+    const isValid = await Tokens.verifyRefreshToken(refreshToken);
     if (!isValid) {
         res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
         return;
@@ -42,7 +42,7 @@ securityRouter.delete("/devices/:id", async (req: RequestWithParams<Params>, res
 
 securityRouter.delete("/devices", async (req: RequestWithParams<Params>, res: Response) => {
     const refreshToken = req.cookies.refreshToken;
-    const isValid = await RefreshToken.verify(refreshToken);
+    const isValid = await Tokens.verifyRefreshToken(refreshToken);
     if (!isValid) {
         res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
         return;
