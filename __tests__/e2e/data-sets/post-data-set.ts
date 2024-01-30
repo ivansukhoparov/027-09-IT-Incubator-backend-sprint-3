f7e96559618d2de41dc1de5245e337742b513a3d
ivansukhoparov <rbcdaaa@gmail.com>
2024-01-31 00:21:33 +0100

    refactoring: move post data sets to specify file

---
+++ __tests__/e2e/data-sets/post-data-set.ts
@@ -0,0 +1,38 @@
+export const routerName = "/posts/";
+export const emptyData = {
+    title: "",
+    shortDescription: "",
+    content: "",
+    blogId: ""
+}
+export const spaceData = {
+    title: "    ",
+    shortDescription: "    ",
+    content: "    ",
+    blogId: "     "
+}
+export const overLengthData = {
+    title: "over_30_aaaaaaaaaaaaaaaaaaaaaaa",
+    shortDescription: "over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__a",
+    content: "over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__over_100__a",
+    blogId: "can't be over length"
+}
+export const validCreateData = {
+    title: "new title",
+    shortDescription: "a very short description",
+    content: "some content",
+    blogId: "testBlogID"
+}
+export const validUpdateData = {
+    title: "update title",
+    shortDescription: "a very short new description",
+    content: "some new content",
+}
+export const emptyBody = {
+    pagesCount: 0,
+    page: 1,
+    pageSize: 10,
+    totalCount: 0,
+    items: []
+
+}
