afb022811aca54a488355c5428ab1ce3881dc74b
ivansukhoparov <rbcdaaa@gmail.com>
2024-02-01 16:37:55 +0100

    refactoring: create comments controller and refactoring comments router

--- src/routers/comments-router.ts
+++ src/routers/comments-router.ts
@@ -8,46 +8,21 @@ import {UpdateCommentDto} from "../types/comments/input";
 import {validateComment} from "../middlewares/validators/comments-validator";
 import {inputValidationMiddleware} from "../middlewares/validators/input-validation-middleware";
 import {CommentsService} from "../domains/comments-service";
+import {CommentsController} from "./controllers/comments-controller";
 
 export const commentsRouter = Router();
+const commentsControllerInstance = new CommentsController();
 
-commentsRouter.get("/:id", async (req: RequestWithParams<Params>, res: Response) => {
-    const comment = await CommentsQueryRepository.getCommentById(req.params.id);
-    if (!comment) {
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
-        return;
-    }
-    res.status(HTTP_STATUSES.OK_200).json(comment);
-})
+commentsRouter.get("/:id", commentsControllerInstance.getComments.bind(commentsControllerInstance));
 
 commentsRouter.put("/:id",
     AuthorizationMiddleware,
     accessRight,
     validateComment,
     inputValidationMiddleware,
-    async (req: RequestWithBodyAndParams<Params, { content: string }>, res: Response) => {
+    commentsControllerInstance.updateComment.bind(commentsControllerInstance));
 
-    const updateData: UpdateCommentDto = {
-            content: req.body.content
-        }
-
-        const isUpdated = await CommentsService.updatePost(updateData, req.params.id,);
-        if (!isUpdated) {
-            res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-            return;
-        }
-        res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-    })
-
-commentsRouter.delete("/:id", AuthorizationMiddleware, accessRight, async (req: RequestWithParams<Params>, res: Response) => {
-    const isDeleted = await CommentsRepository.deleteCommentById(req.params.id);
-    if (isDeleted === null) {
-        res.sendStatus(HTTP_STATUSES.SERVER_ERROR_500);
-        return;
-    }
-    if (!isDeleted) {
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
-        return;
-    }
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-})
+commentsRouter.delete("/:id",
+    AuthorizationMiddleware,
+    accessRight,
+    commentsControllerInstance.deleteComment.bind(commentsControllerInstance));
---
+++ src/routers/controllers/comments-controller.ts
@@ -0,0 +1,57 @@
+import {Params, RequestWithBodyAndParams, RequestWithParams} from "../../types/common";
+import {Response} from "express";
+import {CommentsQueryRepository} from "../../repositories/comments-query-repository";
+import {HTTP_STATUSES} from "../../utils/comon";
+import {UpdateCommentDto} from "../../types/comments/input";
+import {CommentsService} from "../../domains/comments-service";
+import {CommentsRepository} from "../../repositories/comments-repository";
+
+export class CommentsController {
+    private commentsQueryRepository: CommentsQueryRepository;
+    private commentService: CommentsService;
+    private commentsRepository: CommentsRepository;
+
+    constructor() {
+        this.commentsQueryRepository = new CommentsQueryRepository();
+        this.commentService = new CommentsService();
+        this.commentsRepository = new CommentsRepository();
+    }
+
+    async getComments(req: RequestWithParams<Params>, res: Response) {
+        const comment = await this.commentsQueryRepository.getCommentById(req.params.id);
+        if (!comment) {
+            res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+            return;
+        }
+        res.status(HTTP_STATUSES.OK_200).json(comment);
+    }
+
+
+    async updateComment(req: RequestWithBodyAndParams<Params, { content: string }>, res: Response) {
+
+        const updateData: UpdateCommentDto = {
+            content: req.body.content
+        }
+
+        const isUpdated = await this.commentService.updatePost(updateData, req.params.id,);
+        if (!isUpdated) {
+            res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+            return;
+        }
+        res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+    }
+
+    async deleteComment(req: RequestWithParams<Params>, res: Response) {
+        const isDeleted = await this.commentsRepository.deleteCommentById(req.params.id);
+        if (isDeleted === null) {
+            res.sendStatus(HTTP_STATUSES.SERVER_ERROR_500);
+            return;
+        }
+        if (!isDeleted) {
+            res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+            return;
+        }
+        res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+    }
+
+}
