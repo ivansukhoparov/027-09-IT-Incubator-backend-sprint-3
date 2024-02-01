b4eb59c94fed0b5f339f2258c5998dff31110ef6
ivansukhoparov <rbcdaaa@gmail.com>
2024-02-01 16:39:40 +0100

    refactoring: api-request-limit-middleware.ts

--- src/middlewares/security/api-request-limit-middleware.ts
+++ src/middlewares/security/api-request-limit-middleware.ts
@@ -3,11 +3,11 @@ import {ApiRequestsRepository} from "../../repositories/api-requests-repository"
 import {HTTP_STATUSES} from "../../utils/comon";
 import {settings} from "../../settings";
 
-
+const apiRequestsRepository = new ApiRequestsRepository();
 export const apiRequestLimitMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const ip = req.ip || "unknown"
-    await ApiRequestsRepository.writeRequest(ip, req.originalUrl)
-    const requestCount = await ApiRequestsRepository.countRequestByTime(ip, req.originalUrl, settings.requestLimit.interval)
+    await apiRequestsRepository.writeRequest(ip, req.originalUrl)
+    const requestCount = await apiRequestsRepository.countRequestByTime(ip, req.originalUrl, settings.requestLimit.interval)
     if (requestCount > settings.requestLimit.count) {
         res.sendStatus(HTTP_STATUSES.TOO_MANY_REQUESTS_429);
         return;
