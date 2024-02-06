cbd705c4b75e42c0450b6e7791e8f58e65e1df0e
ivansukhoparov <rbcdaaa@gmail.com>
2024-02-07 00:15:12 +0100

    refactoring: add returning array of created blogs in createBlogs

--- __tests__/e2e/utils/create-users.ts
+++ __tests__/e2e/utils/create-users.ts
@@ -1,13 +1,17 @@
 import request from "supertest";
 import {app} from "../../../src/app";
+import {UserOutputType} from "../../../src/types/users/output";
 
 export const createUsers = async (numberOfUsers: number) => {
+	const users:UserOutputType[] =[];
 	for (let i = 1; i <= numberOfUsers; i++) {
 		const createUserData = {
 			login: "user_" + i,
 			email: "user" + i + "@gmail.com",
 			password: "qwerty"
 		};
-		await request(app).post("/users/").auth("admin", "qwerty").send(createUserData);
+		const res = await request(app).post("/users/").auth("admin", "qwerty").send(createUserData);
+		users.push(res.body);
 	}
+	return users;
 };
