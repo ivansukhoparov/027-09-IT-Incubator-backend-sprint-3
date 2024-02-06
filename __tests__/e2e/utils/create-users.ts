2239b04bcace72d4dffd5ea246596830f5bc159e
ivansukhoparov <rbcdaaa@gmail.com>
2024-02-07 00:24:22 +0100

    add: add func creating user with any data

--- __tests__/e2e/utils/create-users.ts
+++ __tests__/e2e/utils/create-users.ts
@@ -1,17 +1,31 @@
 import request from "supertest";
 import {app} from "../../../src/app";
 import {UserOutputType} from "../../../src/types/users/output";
+import {CreateUserType} from "../../../src/types/users/input";
 
 export const createUsers = async (numberOfUsers: number) => {
+
 	const users:UserOutputType[] =[];
 	for (let i = 1; i <= numberOfUsers; i++) {
-		const createUserData = {
+		const createUserData:CreateUserType = {
 			login: "user_" + i,
 			email: "user" + i + "@gmail.com",
 			password: "qwerty"
 		};
 		const res = await request(app).post("/users/").auth("admin", "qwerty").send(createUserData);
+
 		users.push(res.body);
 	}
+
 	return users;
+};
+
+export const createUserWithData = async (createUserData:any) => {
+
+	const res = await request(app)
+		.post("/users/")
+		.auth("admin", "qwerty")
+		.send(createUserData);
+
+	return res.body;
 };
