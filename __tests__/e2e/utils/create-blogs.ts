6b196b886fbd59e6c3d3d6ab1556e6354b082fa5
ivansukhoparov <rbcdaaa@gmail.com>
2024-02-07 00:15:36 +0100

    refactoring: add returning array of created blogs in createBlogs

--- __tests__/e2e/utils/create-blogs.ts
+++ __tests__/e2e/utils/create-blogs.ts
@@ -1,17 +1,20 @@
 import {app} from "../../../src/app";
 import request from "supertest";
+import {BlogOutputType} from "../../../src/types/blogs/output";
 
 
 export const createBlogs = async (numberOfBlogs: number) => {
-
+	const blogs:BlogOutputType[] = [];
 	for (let i = 1; i <= numberOfBlogs; i++) {
 		const createBlogData = {
 			name: "Blog_" + i,
 			description: "some valid description",
 			websiteUrl: "http://www.validurl.com"
 		};
-		await request(app).post("/blogs/")
+		const res = await request(app).post("/blogs/")
 			.auth("admin", "qwerty")
 			.send(createBlogData);
+		blogs.push(res.body);
 	}
+	return blogs;
 };
