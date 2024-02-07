a51e2cf297c8bb4759264d75a9e6e0280aad5209
ivansukhoparov <rbcdaaa@gmail.com>
2024-02-07 01:26:26 +0100

    refactoring: update users test with testingUserCreate

--- __tests__/e2e/users.api.test.ts
+++ __tests__/e2e/users.api.test.ts
@@ -4,7 +4,7 @@ import {app} from "../../src/app";
 import {HTTP_STATUSES} from "../../src/utils/comon";
 import {UserOutputType} from "../../src/types/users/output";
 import {settings} from "../../src/settings";
-import {createUserData, onlySpaces, ViewModelResponse} from "./data-sets/users-dataset";
+import {testUserData, ViewModelResponse} from "./data-sets/users-dataset";
 
 import {testingUserCreate} from "./utils/testing-user-create";
 
@@ -16,6 +16,9 @@ describe(routerName, () => {
 	const mongoURI = settings.env.mongoUri+"/"+settings.env.mongoDbName;
 	beforeAll(async () => {
 		await mongoose.connect(mongoURI); // Connecting to the database.
+		await request(app).delete("/testing/all-data");
+	});
+	beforeEach(async () => {
 		await request(app).delete("/testing/all-data");
 	});
 
@@ -32,40 +35,33 @@ describe(routerName, () => {
 	it(" - POST doesn't create new user with invalid authorization", async () => {
 		await request(app).post(routerName)
 			.auth("Odmin", "qwerty")
-			.send(createUserData.valid.data).expect(HTTP_STATUSES.UNAUTHORIZED_401);
+			.send(testUserData.valid.req).expect(HTTP_STATUSES.UNAUTHORIZED_401);
 	});
 
 	it(" - POST doesn't create new user with empty data", async () => {
-		await request(app).post(routerName)
-			.auth("admin", "qwerty")
-			.send(createUserData.empty.data)
-			.expect(HTTP_STATUSES.BAD_REQUEST_400, createUserData.empty.errors);
+		await testingUserCreate(testUserData.empty);
 	});
-	it(" changed - POST doesn't create new user with spaces", async () => {
-		await testingUserCreate(onlySpaces);
+
+	it(" - POST doesn't create new user with spaces", async () => {
+		await testingUserCreate(testUserData.onlySpaces);
 	});
+
 	it(" - POST doesn't create new user with over length data", async () => {
-		await request(app).post(routerName)
-			.auth("admin", "qwerty")
-			.send(createUserData.overLength.data)
-			.expect(HTTP_STATUSES.BAD_REQUEST_400, createUserData.overLength.errors);
+		await testingUserCreate(testUserData.overLength);
 	});
+
 	it(" - POST doesn't create new user with less then required length data", async () => {
-		await request(app).post(routerName)
-			.auth("admin", "qwerty")
-			.send(createUserData.lessRequireLength.data)
-			.expect(HTTP_STATUSES.BAD_REQUEST_400, createUserData.lessRequireLength.errors);
+		await testingUserCreate(testUserData.lessRequireLength);
 	});
+
 	it(" - POST doesn't create new user with invalid email", async () => {
-		await request(app).post(routerName).auth("admin", "qwerty")
-			.send(createUserData.invalidEmail.data)
-			.expect(HTTP_STATUSES.BAD_REQUEST_400, createUserData.invalidEmail.errors);
+		await testingUserCreate(testUserData.invalidEmail);
 	});
+
 	it(" + POST should create user with valid data and return created user", async () => {
-		const res = await request(app).post(routerName).auth("admin", "qwerty")
-			.send(createUserData.valid.data)
-			.expect(HTTP_STATUSES.CREATED_201);
-		user = res.body;
+		await mongoose.connect(mongoURI); // Connecting to the database.
+		await request(app).delete("/testing/all-data");
+		await testingUserCreate(testUserData.valid);
 	});
 
 	it(" + GET should return all user ", async () => {
