2cf905d2780e42975dfec453ec8fca98c84aae91
ivansukhoparov <rbcdaaa@gmail.com>
2024-02-07 01:26:08 +0100

    refactoring user data set

--- __tests__/e2e/data-sets/users-dataset.ts
+++ __tests__/e2e/data-sets/users-dataset.ts
@@ -1,4 +1,5 @@
 import {HTTP_STATUSES} from "../../../src/utils/comon";
+
 export class ViewModelResponse {
 	static emptyBody = {
 		pagesCount: 0,
@@ -9,77 +10,6 @@ export class ViewModelResponse {
 	};
 }
 
-export class createUserData {
-
-	static valid = {
-		data: {
-			login: "login",
-			email: "qwe123@gmail.com",
-			password: "qwerty"
-		},
-		response: {
-			pagesCount: 0,
-			page: 1,
-			pageSize: 10,
-			totalCount: 0,
-			items: []
-		}
-	};
-
-	static overLength = {
-		data: {
-			login: "login_over_10",
-			email: "qwe123@gmail.com",
-			password: "password_over_20_characters"
-		},
-		errors: this._errorsResponse(["login", "password"])
-	};
-
-	static lessRequireLength = {
-		data: {
-			login: "Lo",
-			email: "qwe123@gmail.com",
-			password: "pas"
-		},
-		errors: this._errorsResponse(["login", "password"])
-	};
-
-	static invalidEmail = {
-		data: {
-			login: "login",
-			email: "qwe",
-			password: "qwerty"
-		},
-		errors: this._errorsResponse(["email"])
-	};
-
-	static empty = {
-		data: {
-			login: "",
-			email: "",
-			password: ""
-		},
-		errors: this._errorsResponse(["login", "email", "password"])
-	};
-
-	static onlySpaces = {
-		data: {
-			login: "     ",
-			email: "     ",
-			password: "       "
-		},
-		errors: this._errorsResponse(["login", "email", "password"])
-	};
-
-	static _errorsResponse(fields: string[]) {
-		const response: { errorsMessages: Object[] } = {errorsMessages: []};
-		fields.forEach(f => {
-			response.errorsMessages.push({message: "Invalid value", field: f});
-		});
-		return response;
-	}
-
-}
 function errorsResponse(fields: string[]) {
 	const response: { errorsMessages: Object[] } = {errorsMessages: []};
 	fields.forEach(f => {
@@ -88,16 +18,67 @@ function errorsResponse(fields: string[]) {
 	return response;
 }
 
-
-
-export const onlySpaces = {
-	req: {
-		login: "        ",
-		email: "     ",
-		password: "       "
+export const testUserData = {
+	onlySpaces: {
+		req: {
+			login: "        ",
+			email: "     ",
+			password: "       "
+		},
+		res: errorsResponse(["login", "email", "password"]),
+		resCode: HTTP_STATUSES.BAD_REQUEST_400
 	},
-	res: errorsResponse(["login", "email", "password"]),
-	resCode: HTTP_STATUSES.BAD_REQUEST_400
+	empty: {
+		req: {
+			login: "",
+			email: "",
+			password: ""
+		},
+		res: errorsResponse(["login", "email", "password"]),
+		resCode: HTTP_STATUSES.BAD_REQUEST_400
+	},
+	invalidEmail: {
+		req: {
+			login: "login",
+			email: "qwe",
+			password: "qwerty"
+		},
+		res: errorsResponse(["email"]),
+		resCode: HTTP_STATUSES.BAD_REQUEST_400
+	},
+	lessRequireLength: {
+		req: {
+			login: "Lo",
+			email: "qwe123@gmail.com",
+			password: "pas"
+		},
+		res: errorsResponse(["login", "password"]),
+		resCode: HTTP_STATUSES.BAD_REQUEST_400
+	},
+	overLength: {
+		req: {
+			login: "login_over_10",
+			email: "qwe123@gmail.com",
+			password: "password_over_20_characters"
+		},
+		res: errorsResponse(["login", "password"]),
+		resCode: HTTP_STATUSES.BAD_REQUEST_400
+	},
+	valid: {
+		req: {
+			login: "login",
+			email: "qwe123@gmail.com",
+			password: "qwerty"
+		},
+		res: {
+			id: expect.any(String),
+			login: expect.any(String),
+			email: expect.any(String),
+			createdAt: expect.any(String)
+		},
+		resCode: HTTP_STATUSES.CREATED_201
+	}
 };
+
 
 
