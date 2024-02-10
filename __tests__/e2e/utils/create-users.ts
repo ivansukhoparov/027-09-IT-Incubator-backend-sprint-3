64005049cb7e26b63c5abb0ce50e99a1e9d894d9
ivansukhoparov <rbcdaaa@gmail.com>
2024-02-10 02:05:20 +0100

    refactoring: update create entities function

--- __tests__/e2e/utils/create-users.ts
+++ __tests__/e2e/utils/create-users.ts
@@ -3,6 +3,76 @@ import {app} from "../../../src/app";
 import {UserOutputType} from "../../../src/types/users/output";
 import {CreateUserType} from "../../../src/types/users/input";
 
+type EntitiesType = "users" | "blogs" | "posts" | "comments" | "likes"
+
+const entitiesFactory = {
+	"users": (i: number, entityName: string = "user") => {
+		return {
+			login: entityName + "_" + i,
+			email: entityName + i + "@gmail.com",
+			password: "qwerty"
+		};
+	},
+	"blogs": (i: number, entityName: string = "user") => {
+		return {
+			login: entityName + "_" + i,
+			email: entityName + i + "@gmail.com",
+			password: "qwerty"
+		};
+	},
+	"posts": (i: number, entityName: string = "user") => {
+		return {
+			login: entityName + "_" + i,
+			email: entityName + i + "@gmail.com",
+			password: "qwerty"
+		};
+	},
+	"comments": (i: number, entityName: string = "user") => {
+		return {
+			login: entityName + "_" + i,
+			email: entityName + i + "@gmail.com",
+			password: "qwerty"
+		};
+	},
+	"likes": (i: number, entityName: string = "user") => {
+		return {
+			login: entityName + "_" + i,
+			email: entityName + i + "@gmail.com",
+			password: "qwerty"
+		};
+	},
+};
+
+export class CreateEntity {
+	private entityType: EntitiesType;
+
+	constructor(EntityType: EntitiesType) {
+		this.entityType = EntityType;
+	}
+
+	async create(numberOfEntities: number, entityName?: string) {
+		const entities: any[] = [];
+		for (let i = 1; i <= numberOfEntities; i++) {
+			let createEntityData: any;
+			if (entityName) {
+				createEntityData = entitiesFactory[this.entityType](i,entityName);
+			} else {
+				createEntityData = entitiesFactory[this.entityType](i);
+			}
+			const res = await request(app)
+				.post("/" + this.entityType + "/")
+				.auth("admin", "qwerty")
+				.send(createEntityData);
+
+			entities.push(res.body);
+
+		}
+		return entities;
+	}
+}
+
+
+
 export const createUsers = async (numberOfUsers: number) => {
 
 	const users:UserOutputType[] =[];
