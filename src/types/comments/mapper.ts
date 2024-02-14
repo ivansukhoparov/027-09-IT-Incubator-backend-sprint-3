0c8d3d6d0ea96727a2a1a03da05967fdaea5ae05
ivansukhoparov <rbcdaaa@gmail.com>
2024-02-14 23:34:00 +0100

    update: update comments mapper with likes

--- src/types/comments/mapper.ts
+++ src/types/comments/mapper.ts
@@ -1,7 +1,7 @@
 import {WithId} from "mongodb";
-import {CommentType, OutputCommentType} from "./output";
+import {CommentType, LikesInfoType, OutputCommentType} from "./output";
 
-export const commentMapper=(input:WithId<CommentType>):OutputCommentType=>{
+export const commentMapper=(input:WithId<CommentType>, likes:LikesInfoType):OutputCommentType=>{
 	return {
 		id: input._id.toString(),
 		content: input.content,
@@ -9,6 +9,7 @@ export const commentMapper=(input:WithId<CommentType>):OutputCommentType=>{
 			userId: input.commentatorInfo.userId,
 			userLogin: input.commentatorInfo.userLogin
 		},
-		createdAt: input.createdAt
+		createdAt: input.createdAt,
+		likesInfo:likes
 	};
 };
